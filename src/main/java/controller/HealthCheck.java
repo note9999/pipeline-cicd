6beb605ee9a13d4518f229dcc826be46b03ce576
note9999 <sanghoon273@gmail.com>
2023-12-05 16:44:43 +0900

    dev,local,prod로 분리

---
+++ src/main/java/controller/HealthCheck.java
@@ -0,0 +1,26 @@
+package controller;
+
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.Status;
+import org.springframework.web.client.RestTemplate;
+
+public class HealthCheck {
+
+    private static final String HEALTH_ENDPOINT_URL = "http://localhost:8080/actuator/health"; // Actuator health 엔드포인트 URL
+
+    public static void main(String[] args) {
+        RestTemplate restTemplate = new RestTemplate();
+
+        // Actuator의 health 엔드포인트를 호출하여 상태를 가져옴
+        Health health = restTemplate.getForObject(HEALTH_ENDPOINT_URL, Health.class);
+
+        if (health != null) {
+            Status status = health.getStatus();
+            System.out.println("Health Status: " + status.getCode()); // 상태 코드 출력
+            System.out.println("Health Details: " + health.getDetails()); // 상세 정보 출력
+        } else {
+            System.out.println("Health check failed!");
+        }
+    }
+}
